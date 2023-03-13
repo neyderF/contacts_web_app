7043e97103eb46f509e700511d117c28e4c72276
Neyder Figueroa Sánchez <49882090+neyderF@users.noreply.github.com>
2023-03-12 23:48:09 -0500

    ++

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,6 @@
 export function Footer() {
 
     return (<footer className="text-center">
-        <p> Desingned by NFS, More info in <a href="https://nfslabs.tech">nfslabs.tech</a></p>
+        <p> Designed by NFS, More info in <a href="https://nfslabs.tech">nfslabs.tech</a></p>
     </footer>);
 }
